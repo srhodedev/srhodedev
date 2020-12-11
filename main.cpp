e4b69a80a7d1568d11468b26a71674317ab52e73
srhodedev <stephan.rhode@orange.fr>
2020-12-11 23:09:08 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Formation à CPP
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+        std::cout << std::setfill('*') << std::setw(20) << "Bonjour !" << std::endl;
+        return 0;
+}
+
